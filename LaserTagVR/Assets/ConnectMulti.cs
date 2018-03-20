364b8ca3b80bf9471eabfa666637461d036761cd
Evan Green <edgreen@ucsd.edu>
2018-03-20 01:24:00 -0700

    Added PUN multiplayer functionality, kind of half works

---
+++ LaserTagVR/Assets/ConnectMulti.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using Photon;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+using ExitGames.Client.Photon;
+
+public class ConnectMulti {
+    public string versionName = "0.1";
+    public GameObject sectionView1, sectionView2, sectionView3;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+
+}
